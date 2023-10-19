7c4f5bba24518fd3f77d5976df6e2d34626f5571
Surendar Singh <surendarsingh2002@gmail.com>
2023-10-19 23:07:14 +0530

    Loader Green Spinner Added

--- components/common/Loader/index.tsx
+++ components/common/Loader/index.tsx
@@ -1,7 +1,17 @@
+import Image from "next/image";
+
 const Loader = () => {
   return (
     <div className="flex h-screen items-center justify-center bg-white">
-      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-primary border-t-transparent"></div>
+      <div className="h-16 w-16 animate-spin rounded-full border-4 border-solid border-findmyleather border-t-transparent"></div>
+      <div className="w-24 h-24 mb-4">
+        {/* <Image
+          src="/images/findmyleather/Logo-Trans-bg.png"
+          alt="logo"
+          height={100}
+          width={100}
+        /> */}
+      </div>
     </div>
   );
 };
