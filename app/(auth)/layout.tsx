7b201cc9e83da1bd391931a61f5af61e2e7a212a
Surendar Singh <surendarsingh2002@gmail.com>
2023-10-19 18:30:11 +0530

    Auth Frontend Created

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,20 +1,15 @@
 "use client";
-import "./globals.css";
-import "./data-tables-css.css";
-import "./satoshi.css";
+import "@/app/globals.css";
+import "@/app/data-tables-css.css";
+import "@/app/satoshi.css";
 import { useState, useEffect } from "react";
 import Loader from "@/components/common/Loader";
-
-import Sidebar from "@/components/Sidebar";
-import Header from "@/components/Header";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const [sidebarOpen, setSidebarOpen] = useState(false);
-
   const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
@@ -29,15 +24,7 @@ export default function RootLayout({
             <Loader />
           ) : (
             <div className="flex h-screen overflow-hidden">
-              <Sidebar
-                sidebarOpen={sidebarOpen}
-                setSidebarOpen={setSidebarOpen}
-              />
               <div className="relative flex flex-1 flex-col overflow-y-auto overflow-x-hidden">
-                <Header
-                  sidebarOpen={sidebarOpen}
-                  setSidebarOpen={setSidebarOpen}
-                />
                 <main>
                   <div className="mx-auto max-w-screen-2xl p-4 md:p-6 2xl:p-10">
                     {children}
--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,16 @@
-import ECommerce from "@/components/Dashboard/E-commerce";
+import Dashboard from "@/components/Dashboard/Dashboard";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "TailAdmin | Next.js E-commerce Dashboard Template",
-  description: "This is Home Blog page for TailAdmin Next.js",
+  title: "Find My Leather",
+  description: "India's first global leather sourcing and procurement platform",
   // other metadata
 };
 
 export default function Home() {
   return (
     <>
-      <ECommerce />
+      <Dashboard />
     </>
   );
 }
