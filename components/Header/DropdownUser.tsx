31124c096322a534715ffc1f526ab585347a5a53
Surendar Singh <surendarsingh2002@gmail.com>
2023-10-23 21:02:38 +0530

    Dashboard header updated to show auth user

--- components/Header/DropdownUser.tsx
+++ components/Header/DropdownUser.tsx
@@ -1,9 +1,11 @@
 import { useEffect, useRef, useState } from "react";
 import Link from "next/link";
 import Image from "next/image";
-import { signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 
 const DropdownUser = () => {
+  const { data: session, status } = useSession();
+
   const [dropdownOpen, setDropdownOpen] = useState(false);
 
   const trigger = useRef<any>(null);
@@ -45,9 +47,9 @@ const DropdownUser = () => {
       >
         <span className="hidden text-right lg:block">
           <span className="block text-sm font-medium text-black dark:text-white">
-            Arshad
+            {session?.user?.name}
           </span>
-          <span className="block text-xs">FindMyLeather</span>
+          <span className="block text-xs">{session?.user?.email}</span>
         </span>
 
         <span className="h-12 w-12 rounded-full">
