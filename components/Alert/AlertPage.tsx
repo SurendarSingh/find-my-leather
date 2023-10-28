ae0271e460dc80da2580656c035c047d9bd0617e
Surendar Singh <surendarsingh2002@gmail.com>
2023-10-29 02:33:03 +0530

    Alert Page edited

--- components/Alert/AlertPage.tsx
+++ components/Alert/AlertPage.tsx
@@ -1,11 +1,7 @@
 "use client";
 
 import React from "react";
-import {
-  SuccessAlert,
-  ErrorAlert,
-  WarningAlert,
-} from "@/components/Alert/Alerts";
+import { toast } from "react-toastify";
 
 const alert = {
   title: "Your Account Successfully Created",
@@ -17,22 +13,28 @@ const AlertPage = () => {
     <div className="rounded-sm border border-stroke bg-white p-4 shadow-default dark:border-strokedark dark:bg-boxdark md:p-6 xl:p-9">
       <div className="flex flex-col gap-7.5">
         <button
-          onClick={() => SuccessAlert(alert)}
+          onClick={() => toast.success(alert.title + "\n" + alert.message)}
           className="text-white bg-meta-3 text-lg font-semibold px-6 py-2.5 rounded-md"
         >
           Success Alert
         </button>
         <button
-          onClick={() => ErrorAlert(alert)}
+          onClick={() => toast.error(alert.title + "\n" + alert.message)}
           className="text-white bg-meta-1 text-lg font-semibold px-6 py-2.5 rounded-md"
         >
           Error Alert
         </button>
         <button
-          onClick={() => WarningAlert(alert)}
+          onClick={() => toast.warn(alert.title + "\n" + alert.message)}
           className="text-white bg-warning text-lg font-semibold px-6 py-2.5 rounded-md"
         >
           Warning Alert
+        </button>
+        <button
+          onClick={() => toast.info(alert.title + "\n" + alert.message)}
+          className="text-white bg-primary text-lg font-semibold px-6 py-2.5 rounded-md"
+        >
+          Info Alert
         </button>
       </div>
     </div>
