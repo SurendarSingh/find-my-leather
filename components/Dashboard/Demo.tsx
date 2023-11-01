2df0110319bba20c89758b4e6ef2540412b76c6c
Surendar Singh <surendarsingh2002@gmail.com>
2023-11-02 05:21:14 +0530

    CardDataStats refactored

--- components/Dashboard/Demo.tsx
+++ components/Dashboard/Demo.tsx
@@ -5,7 +5,7 @@ import ChartThree from "../Charts/ChartThree";
 import ChartTwo from "../Charts/ChartTwo";
 import ChatCard from "../Chat/ChatCard";
 import TableOne from "../Tables/TableOne";
-import CardDataStats from "../CardDataStats";
+import CardDataStats from "./CardDataStats";
 // import Map from "../Maps/TestMap";
 
 // without this the component renders on server and throws an error
